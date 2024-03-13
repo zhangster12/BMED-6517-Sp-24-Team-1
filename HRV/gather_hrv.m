d16a3273f85db10d36c53af93f8b4754a5115732
Vikram Abbaraju <vxa112@case.edu>
2024-03-13 17:09:28 -0400

    Extracted HRV features for all subjects
    Ran HRV feature extraction pipeline to compute 14 2-minute HRV features per stimulus for each subject. The last two minutes of each stimulus were used to compute the features.

--- HRV/gather_hrv.m
+++ HRV/gather_hrv.m
@@ -2,57 +2,67 @@ close all;
 clear all;
 clc;
 
-timing_data_path = '../Timing/';
-feature_data_path = '../Seis_Process_R2/';
+% Path to folder storing RR intervals
+RRinterval_data_path = 'RRintervals/';
+RRinterval_data_folder = dir([RRinterval_data_path 'sub*']);
 
+Fs = 500; % ECG sampling frequency
 
-timing_data_folder = dir([timing_data_path '*.mat']);
+for i = 1:length(RRinterval_data_folder)
 
-for i = 1:length(timing_data_folder)
+    sub_id = RRinterval_data_folder(i).name(1:7);
 
-    sub_id = timing_data_folder(i).name(1:7);
-    timing = load([timing_data_path sub_id '.mat']).timing;
+    HRV_filename = ['HRV_feat_all/' sub_id '_HRV_feat.csv'];
 
-    feature_file = dir([feature_data_path '*' sub_id(4:end) '*.mat']);
+    % Store each HRV feature in a table
+    HRV_feat = table;
 
-    load([feature_data_path feature_file.name]);
+    for j = 0:4
 
-    timing = timing - timing(1);
+        temp = table;
 
-    timing_part1 = timing(1:4);
-    timing_part2 = timing(5:8);
+        % Store the stimulus #
+        temp.stim = j;
 
-    timing = [timing_part1; timing(4); timing(5); timing_part2];
+        % Load the RR intervals
+        RRinterval_filename = [RRinterval_data_path sub_id '/stim' int2str(j) ...
+            '_RRintervals.csv'];
+        RRintervals = readmatrix(RRinterval_filename);
 
-    feature_folder = ['Consolidated_Features/' sub_id];
+        % Isolate the last two minutes of RR interval data for each stimulus
+        RRinterval_t_end = RRintervals(end, 1);
+        RRinterval_t_start = RRinterval_t_end - 120;
+        RRintervals = RRintervals(find( RRinterval_t_start <= RRintervals(:,1) & ... 
+            RRintervals(:,1) <= RRinterval_t_end), 2);
 
-    if ~exist(feature_folder, 'dir')
-        mkdir(feature_folder);
+        HRV_feat_all = RRtoHRVDemo(RRintervals, Fs);
+
+        % Here, I will consolidate HRV_feat_all into a set of features that are
+        % relevant for stress detection
+        
+        % Time-Domain HRV 
+        temp.SDNN = HRV_feat_all(9);
+        temp.RMMSD = HRV_feat_all(10);
+        temp.pnn50 = HRV_feat_all(11);
+        temp.nn50 = HRV_feat_all(12);
+        
+        % Nonlinear HRV
+        temp.SD1 = HRV_feat_all(13);
+        temp.SD2 = HRV_feat_all(14);
+        temp.SDratio = HRV_feat_all(15);
+        
+        % Freq-Domain HRV
+        temp.VLF = HRV_feat_all(20);
+        temp.LF = HRV_feat_all(21);
+        temp.HF = HRV_feat_all(22);
+        temp.LFHF = HRV_feat_all(23);
+        temp.LFn = HRV_feat_all(24);
+        temp.HFn = HRV_feat_all(25);
+        temp.POW = HRV_feat_all(26);
+        
+        HRV_feat = [HRV_feat; temp];
     end
 
-    HR = physFeatures_beat.HR;
-    PPGamp = physFeatures_beat.PPGamp;
-    PAT = physFeatures_beat.PAT;
-    PEP = physFeatures_beat.PEP;
-    PTTrecip = physFeatures_beat.PTT_recip;
-
-    for j = 1:length(timing)/2
-
-        start_time = timing(2*j-1);
-        end_time = timing(2*j);
-
-        HR_seg = HR(find(start_time <= HR(:,1) & HR(:,1) <= end_time), :);
-        PPGamp_seg = PPGamp(find(start_time <= PPGamp(:,1) & PPGamp(:,1) <= end_time), :);
-        PAT_seg = PAT(find(start_time <= PAT(:,1) & PAT(:,1) <= end_time), :);
-        PEP_seg = PEP(find(start_time <= PEP(:,1) & PEP(:,1) <= end_time), :);
-        PTTrecip_seg = PTTrecip(find(start_time <= PTTrecip(:,1) & PTTrecip(:,1) <= end_time), :);
-
-        writematrix(HR_seg, [feature_folder '/stim' int2str(j-1) '_HR.csv']);
-        writematrix(PPGamp_seg, [feature_folder '/stim' int2str(j-1) '_PPGamp.csv']);
-        writematrix(PAT_seg, [feature_folder '/stim' int2str(j-1) '_PAT.csv']);
-        writematrix(PEP_seg, [feature_folder '/stim' int2str(j-1) '_PEP.csv']);
-        writematrix(PTTrecip_seg, [feature_folder '/stim' int2str(j-1) '_PTTrecip.csv']);
-
-    end
+    writetable(HRV_feat, HRV_filename);
 
 end
